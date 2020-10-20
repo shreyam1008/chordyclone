8ba1039386150433a51c0f22805751c43f749e03
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-20 12:33:01 +0545

    clenups

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import { Counter } from './features/counter/Counter';
+
+
+
+const App = () => {
+  return (
+    <div className="app">
+        discord clone
+    </div>
+  )
+}
+
+export default App
