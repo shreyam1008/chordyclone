d6c445f789aebc6fa909568cadf9088be28638f2
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-24 14:48:57 +0545

    adding channels

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
@@ -13,10 +13,31 @@ import SettingsIcon from "@material-ui/icons/Settings";
 import Avatar from "@material-ui/core/Avatar";
 import { selectUser } from "./features/userSlice";
 import { useSelector } from "react-redux";
-import { auth } from "./firebase";
+import db, { auth } from "./firebase";
 
 const Sidebar = () => {
   const user = useSelector(selectUser);
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection("channels").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          channel: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const handleAddChannel = () => {
+    const channelName = prompt("enter new channel");
+    if (channelName) {
+      db.collection("channels").add({
+        channelName: channelName,
+      });
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -32,12 +53,16 @@ const Sidebar = () => {
             <h4>Text channels</h4>
           </div>
 
-          <AddIcon className="sidebar__addChannel" />
+          <AddIcon onClick={handleAddChannel} className="sidebar__addChannel" />
         </div>
         <div className="sidebar__channelsList">
-          <SidebarChannel />
-          <SidebarChannel />
-          <SidebarChannel />
+          {channels.map(({ id, channel }) => (
+            <SidebarChannel
+              key={id}
+              id={id}
+              channelName={channel.channelName}
+            />
+          ))}
         </div>
       </div>
 
--- src/SidebarChannel.jsx
+++ src/SidebarChannel.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import "./SidebarChannel.css"
+import React from "react";
+import "./SidebarChannel.css";
 
-const SidebarChannel = ({id, channel}) => {
-    return (
-        <div className="sidebarChannel">
-            <h4>
-                <span className="sidebarChannel__hash">#</span>
-                Youtube
-            </h4>
-        </div>
-    )
-}
+const SidebarChannel = ({ id, channelName }) => {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannel__hash">#</span>
+        {channelName}
+      </h4>
+    </div>
+  );
+};
 
-export default SidebarChannel
+export default SidebarChannel;
